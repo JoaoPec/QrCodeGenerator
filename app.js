d6e47075cf21b47b03995026897e1a91e5175e5e
João Curcino <Joaopecurcino@hotmail.com>
2023-07-21 15:06:37 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const fs = require("fs")
 
-const path = require("path");
-
 const qr = require("qr-image")
 
 const express = require('express');
@@ -20,21 +18,26 @@ app.get("/", function (req, res) {
     res.sendFile((__dirname + "/index.html"))
 })
 
-
-// Resto do código ...
-
 app.post("/", function (req, res) {
+
     const userPost = req.body.url;
+    
     console.log(userPost);
 
     const qrCode = qr.image(userPost, { type: "png" });
-    const qrSource = "assets/qr-code-image.png"; // Defina o caminho relativo do arquivo
+    const qrSource = "assets/qr-code-image.png"; // 
 
     qrCode.pipe(fs.createWriteStream(qrSource))
         .on("finish", () => {
             console.log("QR code criado com sucesso");
-            const absolutePath = path.join(__dirname, qrSource);
-            res.sendFile(absolutePath); // Envie o arquivo como resposta ao cliente
+
+            res.download(qrSource, "Qr-Code.png", (err) => {
+                if (err) {
+                    console.error('Erro durante o download do arquivo:', err);
+                } else {
+                    console.log('Download concluído com sucesso.');
+                }
+            });
         })
         .on("error", (err) => {
             console.error('Erro ao criar o código QR:', err);
